8e904ca305846322f52d1be99b39427a2fbd9fcf
ArminJaemaa <ArminJaemaa@users.noreply.github.com>
2024-10-03 15:31:55 +0300

    Faili salvestamine külastamine lisatud

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const dtEt = require("./date_time.js");
+const fs = require("fs");
+//päringu lahti arutamiseks post päringute puhuö.
+const bodyparser = require("body-parser");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyparser.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   //res.send("express läks täiesti käima");
-  res.render("index.ejs");
+  res.render("index");
 });
 
 app.get("/timenow", (req, res) => {
@@ -16,4 +20,39 @@ app.get("/timenow", (req, res) => {
   res.render("timenow", { nowWD: weekDays, nowD: dateNow, nowT: timeNow });
 });
 
+app.get("/justlist", (req, res) => {
+  let folkWisdom = [];
+  fs.readFile("public/textfiles/vanasonad.txt", "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      folkWisdom = data.split(";");
+      res.render("justlist", { h2: "vanasõnad", listData: folkWisdom });
+    }
+  });
+});
+app.get("/regvisit", (req, res) => {
+  res.render("regvisit");
+});
+app.post("/regvisit", (req, res) => {
+  console.log(req.body);
+  fs.open("public/textfiles/visitlog.txt", "a", (err, file) => {
+    if (err) {
+      throw err;
+    } else {
+      fs.appendFile(
+        "public/textfiles/visitlog.txt",
+        req.body.firstNameInput + " " + req.body.lastNameInput + ";",
+        (err) => {
+          if (err) {
+            throw err;
+          } else {
+            console.log("faili kirjutati");
+            res.render("regvisit");
+          }
+        }
+      );
+    }
+  });
+});
 app.listen(5133);
