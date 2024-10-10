45263ceac625a067059351fbfd8d67be30ca8247
ArminJaemaa <ArminJaemaa@users.noreply.github.com>
2024-10-10 15:37:44 +0300

    andmebaasiga ühenduse loomine

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dtEt = require("./date_time.js");
 const fs = require("fs");
-//päringu lahti arutamiseks post päringute puhuö.
+const dbInfo = require("../vp1_confiq.js");
+const mysql = require("mysql2");
+//päringu lahti arutamiseks post päringute puhul.
 const bodyparser = require("body-parser");
 
 const app = express();
@@ -11,6 +13,14 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   //res.send("express läks täiesti käima");
   res.render("index");
+});
+
+//andmebaasi ühendus:
+const conn = mysql.createConnection({
+  host: dbInfo.confiqdata.host,
+  user: dbInfo.confiqdata.user,
+  password: dbInfo.confiqdata.passWord,
+  database: dbInfo.confiqdata.dataBase,
 });
 
 app.get("/timenow", (req, res) => {
@@ -45,7 +55,17 @@ app.post("/reqvisit", (req, res) => {
     } else {
       fs.appendFile(
         "public/textfiles/visitlog.txt",
-        req.body.firstNameInput + " " + req.body.lastNameInput + " ( " + wkDays + " " + dateNow + " kell " + timeNow + " )" +  ";",
+        req.body.firstNameInput +
+          " " +
+          req.body.lastNameInput +
+          " ( " +
+          wkDays +
+          " " +
+          dateNow +
+          " kell " +
+          timeNow +
+          " )" +
+          ";",
         (err) => {
           if (err) {
             throw err;
@@ -64,9 +84,70 @@ app.get("/visitlog", (req, res) => {
     if (err) {
       throw err;
     } else {
-      visits = data.split(";")
-      res.render("visitlog", {h3: "külastus", listData: visits})
+      visits = data.split(";");
+      res.render("visitlog", { h3: "külastus", listData: visits });
     }
   });
+});
+app.get("/regvisitdb", (req, res) => {
+  let notice = "";
+  let firstName = "";
+  let lastName = "";
+  res.render("regvisitdb", {
+    notice: notice,
+    firstName: firstName,
+    lastName: lastName,
+  });
+});
+app.post("/regvisitdb", (req, res) => {
+  let notice = "";
+  let firstName = "";
+  let lastName = "";
+  if (!req.body.firstNameInput || !req.body.lastNameInput) {
+    notice = "osa andmeid sisestamata";
+    lastName = req.body.lastNameInput;
+    firstName = req.body.firstNameInput;
+    res.render("regvisitdb", {
+      notice: notice,
+      firstName: firstName,
+      lastName: lastName,
+    });
+  } else {
+    let sqlreq = "INSERT INTO vp1_visitlog (first_name, last_name) VALUES(?,?)";
+    conn.query(
+      sqlreq,
+      [req.body.firstNameInput, req.body.lastNameInput],
+      (err, sqlres) => {
+        if (err) {
+          throw err;
+        } else {
+          notice = "Andmed kirjutati andmebaasi";
+          console.log("andmebaasi kirjutati");
+          res.render("regvisitdb", {
+            notice: notice,
+            firstName: firstName,
+            lastName: lastName,
+          });
+        }
+      }
+    );
+  }
+});
+app.get("/eestifilm", (req, res) => {
+  res.render("film_index");
+});
+app.get("/eestifilm/tegelased", (req, res) => {
+  let sqlReq = "SELECT first_name, last_name, birth_date FROM person";
+  let persons = [];
+  conn.query(sqlReq, (err, sqlres) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(sqlres);
+      persons = sqlres;
+      res.render("tegelased", { persons: persons });
+    }
+  });
+  //res.render("tegelased");
 });
 app.listen(5133);
